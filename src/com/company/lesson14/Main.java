8d3632d94be7dfc056fd9f25f39b67e357316831
TheNimlas <nimals3392@mail.ru>
2021-11-27 17:52:20 +0300

    Init2

--- src/com/company/lesson14/Main.java
+++ src/com/company/lesson14/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         User user = new User("vasa", 21);
         System.out.println(user.toString());
+        System.out.println();
     }
 }
---
+++ src/com/company/lesson14/Test.java
@@ -0,0 +1,4 @@
+package com.company.lesson14;
+
+public class Test {
+}
